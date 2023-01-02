e3c1dfa5c3b739dc86caf6e97ce1f52ef65d699d
lishangli <2840670989@qq.com>
2023-01-02 16:36:59 +0800

    commit change code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 WTSP
+
+## 运行方式
+
+> chmod +x case.sh
+> ./case.sh 用例文件名 用例图结点数 容量C
+
+结果存放在用例文件名.out
--- case.sh
+++ case.sh
@@ -1,14 +1,19 @@
-python3 case.py case.in 10
-python3 case.py case1.in 12
-python3 case.py case2.in 13
-python3 case.py case3.in 25
-python3 case.py case4.in 100
-python3 case.py case5.in 1000
-gcc -o main main.cpp
+#!/bin/bash
+case1=$1
+N=$2
+C=$3
+echo ${case1}
+python3 case.py ${case1}".in" ${N}
+# python3 case.py case1.in 12
+# python3 case.py case2.in 13
+# python3 case.py case3.in 25
+# python3 case.py case4.in 100
+# python3 case.py case5.in 1000
+g++ main.cpp -o main
 
-./main case.in case.out 9999
-./main case1.in case1.out 9999
-./main case2.in case2.out 9999
-./main case3.in case3.out 9999
-./main case4.in case4.out 9999
-./main case5.in case5.out 9999
+./main ${case1}".in" ${case1}".out" ${C}
+# ./main case1.in case1.out 9999
+# ./main case2.in case2.out 9999
+# ./main case3.in case3.out 9999
+# ./main case4.in case4.out 9999
+# ./main case5.in case5.out 9999
